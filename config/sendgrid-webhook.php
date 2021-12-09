5ce198ab6fefc0def818e725154686070d807321
Hleb Prakhnitski <thane.crios@gmail.com>
2021-12-09 14:15:04 +0300

    added new config params for deep message parsing

--- config/sendgrid-webhook.php
+++ config/sendgrid-webhook.php
@@ -7,4 +7,5 @@ return [
     'recipient_attribute' => 'email',
     'sendgrid_webhook_path' => '/sendgrid/webhook',
     'sendgrid_token' => 'token',
+    'sendgrid_messages_api_url' => 'https://api.sendgrid.com/v3/messages/'
 ];
